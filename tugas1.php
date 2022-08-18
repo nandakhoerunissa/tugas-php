895bd2142b734fdc3a2a3fa87990142ce6a7b8ad
nandakhoerunissa <109189382+nandakhoerunissa@users.noreply.github.com>
2022-08-18 07:42:32 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <title>loop for</title>
+</head>
+<body>
+     <?php
+     for ($i = 100; $i >=0; $i--) {
+        if($i % 2 == 1) {
+            echo $i."<br>";
+        }
+     }      
+     ?>              
+</body>
+</html>
---
+++ tugas2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+    <title>loop while</title>
+</head>
+<body>
+    <?php
+   $a =3
+
+   for($i ,$a; $i <= 10;$i++){
+    if($i) {
+        echo pow ($a,$i). "<br>";
+    }
+   }
+   ?>
+</body>
+</html>
